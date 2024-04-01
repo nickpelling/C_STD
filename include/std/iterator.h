9380a59c26557a8c56e879beb47889892c294c33
Nick Pelling <np@remsdaq.com>
2024-04-01 11:42:13 +0100

    Worked around Visual Studio warnings to allow /W4

--- include/std/iterator.h
+++ include/std/iterator.h
@@ -91,6 +91,16 @@ STD_INLINE void stdlib_iterator_construct(std_iterator_t* pstIterator, std_conta
 #define std_iterator_done(IT)		IT.stItBody.stIterator.bDone
 #define std_iterator_at(IT)			STD_ITEM_PTR_CAST(IT, IT.stItBody.stIterator.pvRef)
 
+#define STD_FOR_LOOP(CONTAINER,IT,IT_TYPE,TEMPVAR)	\
+		for (IT_TYPE(CONTAINER) IT, * STD_UNUSED TEMPVAR = (std_iterator_construct(CONTAINER,IT),NULL); \
+			!std_iterator_done(IT); \
+			(TEMPVAR, std_iterator_next(IT))
+
+#define STD_FOR_RANGE_LOOP(CONTAINER,IT,IT_TYPE,BEGIN,END,TEMPVAR)	\
+		for (IT_TYPE(CONTAINER) IT, * STD_UNUSED TEMPVAR = (std_iterator_range(CONTAINER,IT,BEGIN,END),NULL); \
+			!std_iterator_done(IT); \
+			(TEMPVAR, std_iterator_next(IT))
+
 // The container library's generic std_for() macro
 //	- locks the container (if the container has a lockhandler)
 //	- instantiates a named iterator in the inner for-loop scope
@@ -101,17 +111,13 @@ STD_INLINE void stdlib_iterator_construct(std_iterator_t* pstIterator, std_conta
 // Note: this allows break commands in the innermost (iterator) loop,
 // BUT the container will stay locked if you execute a return command
 // inside the inner loop. So don't do this!
-#define std_for(CONTAINER, IT, IS_CONST, IT_TYPE)	\
+#define std_for(CONTAINER, IT, IS_CONST, IT_TYPE)		\
 	std_container_lock_wrapper(CONTAINER, IS_CONST))	\
-		for (IT_TYPE(CONTAINER) IT, * STD_UNUSED STD_FAKEVAR() = (std_iterator_construct(CONTAINER,IT),NULL); \
-			!std_iterator_done(IT); \
-			std_iterator_next(IT)
+		STD_FOR_LOOP(CONTAINER,IT,IT_TYPE,STD_FAKEVAR())
 
 #define std_for_range(CONTAINER, IT, IS_CONST, IT_TYPE, BEGIN, END)	\
-	std_container_lock_wrapper(CONTAINER, IS_CONST))	\
-		for (IT_TYPE(CONTAINER) IT, * STD_UNUSED STD_FAKEVAR() = (std_iterator_range(CONTAINER,IT,BEGIN,END),NULL); \
-			!std_iterator_done(IT); \
-			std_iterator_next(IT)
+	std_container_lock_wrapper(CONTAINER, IS_CONST))				\
+		STD_FOR_RANGE_LOOP(CONTAINER, IT, IT_TYPE, BEGIN, END, STD_FAKEVAR())
 
 #define std_each_forward(CONTAINER, IT)			std_for(CONTAINER, IT, false, std_forward_iterator)
 #define std_each_forward_const(CONTAINER, IT)	std_for(CONTAINER, IT, true,  std_forward_const_iterator)
